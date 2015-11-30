01a807eb4b74ef3b948a197cd0b9b735a4419e3f
Manjunath <manju05cs402@yahoo.co.in>
2015-12-01 01:05:06 +0530

    UI Update as per design

---
+++ TheBureau/TheBureau/ViewControllers/AccountCreationViewController/BUAccountCreationVC.h
@@ -0,0 +1,13 @@
+//
+//  BUAccountCreationVC.h
+//  TheBureau
+//
+//  Created by Manjunath on 01/12/15.
+//  Copyright © 2015 Bureau. All rights reserved.
+//
+
+#import "BUBaseViewController.h"
+
+@interface BUAccountCreationVC : BUBaseViewController
+
+@end
